ea80ac9c50b00f09a573f92814412c2c9cd9d088
lavenderses <nakanoinryota@gmail.com>
2024-05-05 22:20:42 +0900

    Add AppConfigValue for object type

---
+++ src/main/java/io/github/lavenderses/aws_app_config_openfeature_provider/app_config_model/AppConfigObjectValue.java
@@ -0,0 +1,32 @@
+package io.github.lavenderses.aws_app_config_openfeature_provider.app_config_model;
+
+import dev.openfeature.sdk.Value;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import org.intellij.lang.annotations.Language;
+import org.jetbrains.annotations.NotNull;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Boolean type in AWS AppConfig's Attribute.<br/>
+ * This feature flag will be mapped as boolean in OpenFeature requirements.
+ */
+@Getter
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class AppConfigObjectValue extends AppConfigValue<Value> {
+
+    AppConfigObjectValue(
+        @NotNull Boolean enabled,
+        @NotNull Value value,
+        @Language("json") @NotNull String jsonFormat
+    ) {
+        super(
+            /* enabled = */ requireNonNull(enabled, "enabled"),
+            /* value = */ requireNonNull(value, "value"),
+            /* jsonFormat = */ requireNonNull(jsonFormat, "jsonFormat")
+        );
+    }
+}
