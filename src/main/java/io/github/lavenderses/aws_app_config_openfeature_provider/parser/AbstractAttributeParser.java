f9516e5729a969a79f2d423ae9e44d3d9b18b21b
lavenderses <nakanoinryota@gmail.com>
2024-05-06 13:40:23 +0900

    Share Json schema -> `flag_value` extraction logic in abstract

--- src/main/java/io/github/lavenderses/aws_app_config_openfeature_provider/parser/AbstractAttributeParser.java
+++ src/main/java/io/github/lavenderses/aws_app_config_openfeature_provider/parser/AbstractAttributeParser.java
@@ -1,15 +1,24 @@
 package io.github.lavenderses.aws_app_config_openfeature_provider.parser;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.IntNode;
 import com.fasterxml.jackson.databind.node.JsonNodeType;
 import io.github.lavenderses.aws_app_config_openfeature_provider.app_config_model.AppConfigValue;
 import io.github.lavenderses.aws_app_config_openfeature_provider.app_config_model.AppConfigValueKey;
 import io.github.lavenderses.aws_app_config_openfeature_provider.evaluation_value.EvaluationResult;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.VisibleForTesting;
 
 import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
 
+/**
+ * Common logic implemented parser interface. These logic are shared to all type of feature flag.
+ *
+ * @param <T> feature flag type in OpenFeature requirements, such as boolean
+ * @param <V> {@link T}-typed {@link AppConfigValue}. this will be returned.
+ */
 abstract class AbstractAttributeParser<T, V extends AppConfigValue<T>> implements AttributeParser<T, V> {
 
     /**
@@ -39,5 +48,61 @@ abstract class AbstractAttributeParser<T, V extends AppConfigValue<T>> implement
                 /* evaluationResult = */ EvaluationResult.ATTRIBUTE_TYPE_MISMATCH
             );
         }
+    }
+
+    /**
+     * Get feature flag value node ({@code "$.{key_name}.flag_value"} in JSON).<br/>
+     * e.g. When AWS AppConfig is following (feature flag key name is {@code flag_key}, and feature flag value is
+     * number type {@code 12345})...
+     * <pre>
+     * {@code
+     * {
+     *     "flag_key": {
+     *         "enabled": true,
+     *         "flag_value": 12345
+     *     }
+     * }}
+     * </pre>
+     * Parameter keyNode is the {@link JsonNode} of following JSON string.
+     * <pre>
+     * {@code
+     * {
+     *     "enabled": true,
+     *     "flag_value": 12345
+     * }
+     * }
+     * </pre>
+     * Return value is the {@link IntNode} with value {@code 123245}.
+     *
+     * @param expectedNodeType expected `flag_value` node type. if this is null, type check of {@code flag_value} node
+     *                         won't be done.
+     * @throws AppConfigValueParseException if {@param keyNode} is invalid schema
+     */
+    @NotNull
+    protected final JsonNode getValidFlagValueNode(
+        @NotNull final JsonNode keyNode,
+        @Nullable final JsonNodeType expectedNodeType
+    ) {
+        final JsonNode flagValueNode = keyNode.get(AppConfigValueKey.FLAG_VALUE.getKey());
+
+        // If `enabled` is not exists, this is schema error
+        if (isNull(flagValueNode) || flagValueNode.isNull()) {
+            throw new AppConfigValueParseException(
+                /* response = */ keyNode.toString(),
+                /* errorMessage = */ "`flag_value` should exist",
+                /* evaluationResult = */ EvaluationResult.INVALID_ATTRIBUTE_FORMAT
+            );
+        }
+
+        // if `flag_value` is not expected type, this is type error
+        if (nonNull(expectedNodeType) && flagValueNode.getNodeType() != expectedNodeType) {
+            throw new AppConfigValueParseException(
+                /* response = */ keyNode.toString(),
+                /* errorMessage = */ "`flag_value` value expected to be boolean",
+                /* evaluationResult = */ EvaluationResult.ATTRIBUTE_TYPE_MISMATCH
+            );
+        }
+
+        return flagValueNode;
     }
 }
