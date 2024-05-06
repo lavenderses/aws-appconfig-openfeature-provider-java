54c934a29737cfc00fb5be9fa24fcf3ab63715a0
lavenderses <nakanoinryota@gmail.com>
2024-05-06 13:41:40 +0900

    Use shared `flag_value` extraction logic in extended class

--- src/main/java/io/github/lavenderses/aws_app_config_openfeature_provider/parser/BooleanAttributeParser.java
+++ src/main/java/io/github/lavenderses/aws_app_config_openfeature_provider/parser/BooleanAttributeParser.java
@@ -3,11 +3,7 @@ package io.github.lavenderses.aws_app_config_openfeature_provider.parser;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.JsonNodeType;
 import io.github.lavenderses.aws_app_config_openfeature_provider.app_config_model.AppConfigBooleanValue;
-import io.github.lavenderses.aws_app_config_openfeature_provider.app_config_model.AppConfigValueKey;
-import io.github.lavenderses.aws_app_config_openfeature_provider.evaluation_value.EvaluationResult;
 import org.jetbrains.annotations.NotNull;
-
-import static java.util.Objects.isNull;
 
 /**
  * Parser implementation for boolean-type feature flag value.
@@ -26,27 +22,15 @@ public final class BooleanAttributeParser extends AbstractAttributeParser<Boolea
         @NotNull JsonNode responseNode,
         @NotNull JsonNode keyNode
     ) {
-        final JsonNode flagValueNode = keyNode.get(AppConfigValueKey.FLAG_VALUE.getKey());
-        if (isNull(flagValueNode) || flagValueNode.isNull()) {
-            throw new AppConfigValueParseException(
-                /* response = */ keyNode.toString(),
-                /* errorMessage = */ "`flag_value` should exist",
-                /* evaluationResult = */ EvaluationResult.INVALID_ATTRIBUTE_FORMAT
-            );
-        }
+        final JsonNode flagValueNode = getValidFlagValueNode(
+            /* keyNode = */ keyNode,
+            /* expectedNodeType = */ JsonNodeType.BOOLEAN
+        );
 
-        if (flagValueNode.getNodeType() == JsonNodeType.BOOLEAN) {
-            return new AppConfigBooleanValue(
-                /* enabled = */ enabled(keyNode),
-                /* value = */ flagValueNode.asBoolean(),
-                /* responseNode = */ responseNode.toString()
-            );
-        } else {
-            throw new AppConfigValueParseException(
-                /* response = */ keyNode.toString(),
-                /* errorMessage = */ "`flag_value` value expected to be boolean",
-                /* evaluationResult = */ EvaluationResult.ATTRIBUTE_TYPE_MISMATCH
-            );
-        }
+        return new AppConfigBooleanValue(
+            /* enabled = */ enabled(keyNode),
+            /* value = */ flagValueNode.asBoolean(),
+            /* responseNode = */ responseNode.toString()
+        );
     }
 }
